bc53d60e9d51b33948a86a89e3e592563cfcd58c
Afia Yeboah <afia.yakuamoah@gmail.com>
2024-08-27 22:45:30 +0000

    add afia's phone number

--- auto-spareparts-frontend/src/components/Footer.js
+++ auto-spareparts-frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
       <p>Contact us:</p>
       <ul>
         <li>Email: aatandoh56@gmail.com</li>
-        <li>Phone: (+233 545720183)</li>
+        <li>Phone: (+233 545720183), (+233246827566)</li>
         <li>Address: (Accra-Ghana)</li>
       </ul>
     </div>
