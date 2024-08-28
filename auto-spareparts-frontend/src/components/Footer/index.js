46c3717c432d357259fdf290ae0e7253e502c4fb
Afia Yeboah <afia.yakuamoah@gmail.com>
2024-08-28 00:24:57 +0000

    footer style modifications

--- auto-spareparts-frontend/src/components/Footer/index.js
+++ auto-spareparts-frontend/src/components/Footer/index.js
@@ -5,15 +5,17 @@ import Styles from './styles';
 const Footer = () => (
   <Styles className="footer">
     <div className="footer-wrapper">
-      <p>&copy; 2024 Auto & Spare Parts Shop</p>
       <div className="contact-info">
-        <p>Contact us:</p>
-        <ul>
+        <h3 className="title">Contact Us</h3>
+        <ul className="contact-info">
           <li>Email: aatandoh56@gmail.com | afia.yakuamoah@gmail.com</li>
-          <li>Phone: (+233 545720183), (+233246827566)</li>
+          <li>Phone: (+233 545720183), (+233 246827566)</li>
           <li>Address: (Accra-Ghana)</li>
         </ul>
       </div>
+    </div>
+    <div className="copyright">
+      &copy; {new Date().getFullYear()} Auto Spare Parts Shop
     </div>
   </Styles>
 );
